bb3aa086083e54fd82f453add063829d83cdba1f
MargarethaMeyliaHerde <147149282+MargarethaMeyliaHerde@users.noreply.github.com>
2023-11-18 01:51:04 +0700

    Add files via upload

---
+++ Task-OOP-Kotlin-MorningB-Meylia/Task-OOP-Kotlin-MorningB-Meylia/src/main/kotlin/KotlinSubmission-Morning/exercise4/App.kt
@@ -0,0 +1,20 @@
+package id.infinitelearning.KotlinSubmission.exercise4
+
+fun main() {
+    try {
+        val result = divideNumbers(10, 0)
+        println("Result: $result")
+    } catch (e: ArithmeticException) {
+        println("Error: ${e.message}")
+    }
+
+}
+    /**
+     * Buatlah contoh code penggunaan try catch untuk menangani suatu kasus sesuai keinginan teman-teman
+     *
+     */
+    // Buat di bawah sini
+    fun divideNumbers(dividend: Int, divisor: Int): Int {
+        return dividend / divisor
+    }
+
